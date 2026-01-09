562990c38e4943b09b5767501a3d29b96ee3396a
Caitlin <st20338113@outlook.cardiffmet.ac.uk>
2026-01-09 20:01:39 +0000

    1

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -3,11 +3,16 @@
 
 // Function prototypes
 
+void myUpdate(GLFWwindow*window,double tdelta);
+
+void myKeyboardHandler(GLFWwindow*window,int key, int scancode, int action, int mods);
 
 // Global Variables
 
 
-int main(void) {
+int main(void) 
+//main function body
+{
 
 	// Initialise the engine (create window, setup OpenGL backend)
 	int initResult = engineInit("GDV4002 - Applied Maths for Games", 1024, 1024);
@@ -34,12 +39,35 @@ addObject("enemy", glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resour
 
 
 listGameObjectKeys();
+listObjectCounts();
+
+float enemyPhase[3] = { 0.0f, 0.0f, 0.0f };
+float enemyPhaseVelocity[3] = {glm::radians(90.0f),glm::radians(90.0f),glm::radians(90.0f) };
+
+
+setUpdateFunction(myUpdate);
+setKeyboardHandler(myKeyboardHandler);
+
+//check if check was just pressed 
+if (action == GLFW_PRESS) {
+	
+	// now check which key was pressed 
+	switch (key)
+	{
+		case GLFW_KEY_ESCAPE:
+		//If escape is pressed tell GLFW we want to close the window (and quit)
+		
+		glfwSetWindowShouldClose()
+	}
+}
+
 
 	
 	//
 	// Set callback functions
 	//
-	
+
+
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
@@ -49,4 +77,20 @@ listGameObjectKeys();
 
 	// return success :)
 	return 0;
+}
+
+
+
+
+void myUpdate(GLFWwindow* window, double tDelta);{
+
+	GameObjectCollection enemies = getObjectCollection("enemy");
+
+	for (int i = 0; i < enemies.objectCount i++) {
+
+		enemies.objectArray[i]->position.y = sinf(enemyPhase(i)); //assume phase stored in radians so no conversion needed 
+
+		enemyPhase[i] += enemyPhaseVelocity[i] * tDelta;
+	}
+
 }
