df1ff459bf29520fd83dc45d594d0d0c7bb75c04
Caitlin <st20338113@outlook.cardiffmet.ac.uk>
2026-01-10 20:26:20 +0000

    Keyboard inputs

---
+++ gdv4002-base1/gdv4002-base1/Keys.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//define key constants - these are bit numbers in the following keys bitset
+enum key {W, A, S, D, SPACE};
--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,14 +1,15 @@
 #include "Engine.h"
-
+#include "Keys.h"
+#include <bitset>
 
 // Function prototypes
 
 void myUpdate(GLFWwindow* window, double tDelta);
-
-void myKeyboardHandler(GLFWwindow*window,int key, int scancode, int action, int mods);
+void myKeyboardHandler(GLFWwindow*window, int key, int scancode, int action, int mods);
 
 // Global Variables
 
+std::bitset<5> keys{ 0x0 };
 
 int main(void) {
 //main function body
@@ -38,16 +39,13 @@ int main(void) {
 	addObject("enemy", glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
 
 
-
-
-	
 	//
 	// Set callback functions
 	//
 
 	setUpdateFunction(myUpdate);
-
-
+	setKeyboardHandler(myKeyboardHandler);
+	
 	listGameObjectKeys();
 	listObjectCounts();
 
@@ -67,23 +65,61 @@ float enemyPhaseVelocity[3] = { glm::radians(90.0f), glm::radians(90.0f), glm::r
 void myUpdate(GLFWwindow* window, double tDelta) {
 
 GameObjectCollection enemies = getObjectCollection("enemy");
-
 for (int i = 0; i < enemies.objectCount; i++){
-
 enemies.objectArray[i]->position.y = sinf(enemyPhase[i]); //assume phase stored in radians so no conversion needed
-
 enemyPhase[i] += enemyPhaseVelocity[i]*tDelta;
+
+static float playerSpeed = 1.0f;//distance per second 
+GameObject2D* player = getObject("player");
+
+if (keys.test(key::W) == true) {player->position.y += playerSpeed * (float)tDelta;
+if (keys.test(key::S) == true) {player->position.y += playerSpeed * (float)tDelta;
+
+}
 
 //float player1RotationSpeed=glm::radians(90.0f);
 //GameObject2D*player1=getObject("player1");
 //player1->orientation = player1->orientation + glm::radians(90.0)*tDelta;
 }
+
 }
 
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	// check if the key was just pressed 
+	if (action == GLFW_PRESS) {
+		 
+		 //now check which key was pressed 
+		switch (key)
+		{
+			case GLFW_KEY_ESCAPE:
+				//If esacpe is pressed tell GLFW we want to close the window (and quit)
+				glfwSetWindowShouldClose(window,true);
+				break;
 
+			case GLFW_KEY_W:
+				keys[key::W] = true;
+				break;
 
+			case GLFW_KEY_S:
+				keys[key::S] = true;
+				break;
+			}
+		
+		}
+		// if not pressed, check the key has just been released 
+		else if (action == GLFW_RELEASE) {
 
+			//handle key release events 
+		switch (key)
+		{
+			case GLFW_KEY_W:
+			keys[key::W] = false;
+			break;
 
-
-
+			case GLFW_KEY_S:
+				keys[key::S] = false;
+				break;
+			}
+		 }
+	}
 
