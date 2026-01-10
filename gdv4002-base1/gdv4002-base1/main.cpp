ddbfa74548df15ae868d2aca72a6506276372a7d
Caitlin <st20338113@outlook.cardiffmet.ac.uk>
2026-01-10 17:21:14 +0000

    enemy

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -3,16 +3,15 @@
 
 // Function prototypes
 
-void myUpdate(GLFWwindow*window,double tdelta);
+void myUpdate(GLFWwindow* window, double tDelta);
 
 void myKeyboardHandler(GLFWwindow*window,int key, int scancode, int action, int mods);
 
 // Global Variables
 
 
-int main(void) 
+int main(void) {
 //main function body
-{
 
 	// Initialise the engine (create window, setup OpenGL backend)
 	int initResult = engineInit("GDV4002 - Applied Maths for Games", 1024, 1024);
@@ -24,50 +23,21 @@ int main(void)
 		return initResult; // exit if setup failed
 	}
 
+
 	//
 	// Setup game scene objects here
-	//
+	
+	//addObject("player1", glm::vec2(-1.5f, 1.0f), glm::radians(-30.0f), glm::vec2(1.0f, 1.0f), "Resources\\Textures\\bumblebee.png");
+	//addObject("player2", glm::vec2(-1.5f, 1.0f), glm::radians(45.0f), glm::vec2(1.0f, 1.0f), "Resources\\Textures\\bumblebee.png");
+
+
 	addObject("player", glm::vec2(-1.5f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), "Resources\\Textures\\player1_ship.png");
 
-addObject("enemy", glm::vec2(0.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
-
-addObject("enemy", glm::vec2(1.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
-
-addObject("enemy", glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+	addObject("enemy", glm::vec2(0.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+	addObject("enemy", glm::vec2(1.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
+	addObject("enemy", glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.75f, 0.75f), "Resources\\Textures\\alien01.png");
 
 
-
-
-listGameObjectKeys();
-listObjectCounts();
-
-float enemyPhase[3] = { 0.0f, 0.0f, 0.0f };
-float enemyPhaseVelocity[3] = {glm::radians(90.0f),glm::radians(90.0f),glm::radians(90.0f) };
-
-
-setUpdateFunction(myUpdate);
-setKeyboardHandler(myKeyboardHandler);
-
-//check if check was just pressed 
-if (action == GLFW_PRESS) {
-	
-	// now check which key was pressed 
-	switch (key)
-	{
-		case GLFW_KEY_ESCAPE:
-		//If escape is pressed tell GLFW we want to close the window (and quit)
-		
-		glfwSetWindowShouldClose(window,true);
-		break;
-	}
-}
-//if not pressed, check the key has just been released
-else if (action == GLFW_RELEASE) {
-
-// handle key release events
-
-	}
-}
 
 
 	
@@ -75,7 +45,11 @@ else if (action == GLFW_RELEASE) {
 	// Set callback functions
 	//
 
+	setUpdateFunction(myUpdate);
 
+
+	listGameObjectKeys();
+	listObjectCounts();
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
@@ -87,18 +61,22 @@ else if (action == GLFW_RELEASE) {
 	return 0;
 }
 
+float enemyPhase[3] = { 0.0f, 0.0f, 0.0f }; 
+float enemyPhaseVelocity[3] = { glm::radians(90.0f), glm::radians(90.0f), glm::radians(90.0f) };
 
+void myUpdate(GLFWwindow* window, double tDelta) {
 
+GameObjectCollection enemies = getObjectCollection("enemy");
 
-void myUpdate(GLFWwindow* window, double tDelta);{
-
-	GameObjectCollection enemies = getObjectCollection("enemy");
-
-	for (int i = 0; i < enemies.objectCount i++) {
-
-		enemies.objectArray[i]->position.y = sinf(enemyPhase(i)); //assume phase stored in radians so no conversion needed 
-
-		enemyPhase[i] += enemyPhaseVelocity[i] * tDelta;
-	}
-
+//float player1RotationSpeed=glm::radians(90.0f);
+//GameObject2D*player1=getObject("player1");
+//player1->orientation = player1->orientation + glm::radians(90.0)*tDelta;
 }
+
+
+
+
+
+
+
+
