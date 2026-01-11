a406bf96027f2c472d4a662d10690be0e5d522c5
Caitlin <st20338113@outlook.cardiffmet.ac.uk>
2026-01-11 20:05:41 +0000

    ????

---
+++ gdv4002-base1/gdv4002-base1/Emitter.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "GameObject2D.h"
+#include <random>
+
+//class Emitter : public GameObject2D
+//{
+//private:
+	//float emitTimeInterval;
+	//float emitCounter;
+
+	//unsigned long long particleNumber; // monotonically increasing particle index / number - used to set key 
+
+	//GLuint snowflakes[8];
+
+	//random number generator 
+	///std::mt19937 gen;
+
+	//random number distributions 
+	//std::uniform_int_distribution<int> spriteDist; //random integer for particle sprite selection
+	//std::uniform_real_distribution<float> normDist; //-1 to 1
+	//std::uniform_real_distribution<float> massDist, scaleDist;
+
+	//public:
+
+		//Emitter(glm::vec2 initPosition, glm::vec2 initSize, float emitTimeInterval);
+
+		//void update(double tDelta) override;
+		//void render() override;
+
+		
+//};
